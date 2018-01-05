20dea4cde0b18f225b4eace5a4ca7ff91a6b9de9
kvlinden <kvlinden@calvin.edu>
2018-01-05 13:31:39 +0700

    Added photo capture feature to the EntryActivity page - Will need to: - format the basic button included on the activity page. - add an indication of the new photo in the entry feature list. - store the BMP in the local database record for this lexical entry.

--- app/src/main/java/org/sil/gatherwords/EntryActivity.java
+++ app/src/main/java/org/sil/gatherwords/EntryActivity.java
@@ -1,7 +1,14 @@
 package org.sil.gatherwords;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class EntryActivity extends Activity {
 
@@ -10,4 +17,29 @@ public class EntryActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    /**
+     * If there is an image capture activity out there, dispatch an intent to take a picture.
+     *
+     * @param view the view from which the photo request originated
+     */
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Handle the result of a photo-taking activity.
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // TODO: Store the bitmap.
+        }
+    }
+
 }
