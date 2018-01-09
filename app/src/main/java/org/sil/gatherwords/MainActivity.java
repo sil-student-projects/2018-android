6868dc7bfaf6406a94f9fdd1edc5ae897e58ced5
catfinch <steve.sykora@gmail.com>
2018-01-09 09:48:18 +0700

    Create Async access class for database
    Fixes #32

--- app/src/main/java/org/sil/gatherwords/MainActivity.java
+++ app/src/main/java/org/sil/gatherwords/MainActivity.java
@@ -1,8 +1,8 @@
 package org.sil.gatherwords;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
