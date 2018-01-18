e5ffbcc1568b9b59c76f5ded071a4a08373a00d9
alpha0010 <ngcoder@live.com>
2018-01-17 23:39:00 -0500

    Entry: Move image into ListView
    Enables better scrolling

--- app/src/main/java/org/sil/gatherwords/room/FilledWord.java
+++ app/src/main/java/org/sil/gatherwords/room/FilledWord.java
@@ -1,12 +1,14 @@
 package org.sil.gatherwords.room;
 
 import android.arch.persistence.room.Relation;
+import android.graphics.Bitmap;
 
 import java.util.List;
 
 public class FilledWord {
     public long id;
     public long sessionID;
+    public Bitmap picture;
     @Relation(parentColumn = "id", entityColumn = "wordID")
     public List<Meaning> meanings;
 }
